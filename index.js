4ba45c9fea1b2e149cf1bee36670ccd2bbfa204c
donde <donde@dondes-MacBook-Pro.local>
2025-02-13 15:36:33 +0000

    hehe

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function skill_Setup()
     for(let i = 0; i < x.length; i++)
     {
         const amount = x[i].getAttribute("amount");
-        x[i].firstChild. += " - "+ amount+ "%";
+        x[i].firstElementChild.textContent += " - "+ amount+ "%";
         x[i].querySelector(".sliderfill").style.width = amount + "%";
         console.log(amount);
     }
